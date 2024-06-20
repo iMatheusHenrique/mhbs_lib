c3985fb89d0f1b6341d0f4d16876884195e232d4
mhsantos <matheus.henriquesb@hotmail.com>
2024-06-20 01:28:00 -0300

    feat init of project

--- mhbs_lib/utils/exceptions.py
+++ mhbs_lib/utils/exceptions.py
@@ -41,3 +41,42 @@ class NotValidOverwriteSchemaOption(Exception):
     >>>     )
     """
     pass
+
+
+
+def check_table_empty(table_data):
+    """
+    Check if the given table data is empty and raise an exception if it is.
+
+    :param table_data: Data of the table to be checked.
+    :type table_data: list
+    :raises TableIsEmptyError: If the table data is empty.
+    """
+    if not table_data:
+        raise TableIsEmptyError("The table is empty and cannot be processed.")
+
+def validate_save_mode(save_mode):
+    """
+    Validate the table save mode and raise an exception if it is invalid.
+
+    :param save_mode: The save mode to be validated.
+    :type save_mode: str
+    :raises NotValidTableSaveMode: If the save mode is not valid.
+    """
+    valid_save_modes = ['append', 'overwrite']
+    if save_mode not in valid_save_modes:
+        raise NotValidTableSaveMode(f"The save mode '{save_mode}' is not valid.")
+
+def validate_overwrite_schema_option(overwrite_schema):
+    """
+    Validate the overwrite schema option and raise an exception if it is invalid.
+
+    :param overwrite_schema: The overwrite schema option to be validated.
+    :type overwrite_schema: bool
+    :raises NotValidOverwriteSchemaOption: If the overwrite schema option is not valid.
+    """
+    valid_options = [True, False]
+    if overwrite_schema not in valid_options:
+        raise NotValidOverwriteSchemaOption(
+            f"The overwrite schema option '{overwrite_schema}' is not valid."
+        )
