2083e0d79381f10fe5fd6a4e8b604a569f510bdd
mhsantos <matheus.henriquesb@hotmail.com>
2024-06-20 01:27:14 -0300

    feat init of project

--- mhbs_lib/utils/exceptions.py
+++ mhbs_lib/utils/exceptions.py
@@ -1,8 +1,43 @@
 class TableIsEmptyError(Exception):
+    """
+    Exception raised when a table is found to be empty.
+
+    This exception is used to indicate that the operation cannot proceed
+    because the specified table is empty.
+
+    :example:
+    >>> if table_is_empty:
+    >>>     raise TableIsEmptyError("The table is empty and cannot be processed.")
+    """
     pass
 
 class NotValidTableSaveMode(Exception):
+    """
+    Exception raised for invalid table save modes.
+
+    This exception is used to indicate that the provided save mode for
+    a table operation is not valid. Valid save modes might include 'append'
+    and 'overwrite'.
+
+    :example:
+    >>> save_mode = 'invalid_mode'
+    >>> if save_mode not in ['append', 'overwrite']:
+    >>>     raise NotValidTableSaveMode(f"The save mode '{save_mode}' is not valid.")
+    """
     pass
 
 class NotValidOverwriteSchemaOption(Exception):
+    """
+    Exception raised for invalid overwrite schema options.
+
+    This exception is used to indicate that the provided option for overwriting
+    the schema is not valid. Valid options might include True and False.
+
+    :example:
+    >>> overwrite_schema = 'invalid_option'
+    >>> if overwrite_schema not in [True, False]:
+    >>>     raise NotValidOverwriteSchemaOption(
+    >>>         f"The overwrite schema option '{overwrite_schema}' is not valid."
+    >>>     )
+    """
     pass
